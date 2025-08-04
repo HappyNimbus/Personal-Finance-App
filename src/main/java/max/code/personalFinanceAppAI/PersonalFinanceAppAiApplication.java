4e6364fb72383ef05bb6dacd7a3a361874009584
Maksim Kolotev <maxrocks@my.yorku.ca>
2025-08-04 13:46:58 -0400

    initial commit

---
+++ src/main/java/max/code/personalFinanceAppAI/PersonalFinanceAppAiApplication.java
@@ -0,0 +1,13 @@
+package max.code.personalFinanceAppAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalFinanceAppAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalFinanceAppAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/max/code/personalFinanceAppAI/PersonalFinanceAppAiApplicationTests.java
@@ -0,0 +1,13 @@
+package max.code.personalFinanceAppAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinanceAppAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
